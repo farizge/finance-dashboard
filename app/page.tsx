579cdee9594f551b46047e9b541e23150b7c4158
farizge <farizghani11@gmail.com>
2023-12-28 01:19:47 +0700

    Card Balance Statistic

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+import Logo from "@/components/navbar/Logo";
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
-import { Search } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Search, ChevronDown } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="py-4 px-8">
+        <div className="py-4 px-8 flex flex-col gap-8">
             <header className="flex justify-between items-center">
                 <div className="flex flex-col">
                     <h1 className="font-bold text-2xl">Hello, Jhon .D</h1>
@@ -15,10 +22,35 @@ export default function Home() {
                     <Search color="black" />
                 </Button>
             </header>
-            <div>
-                <Card>
+            <div className="flex gap-4 max-w-full">
+                <Card className="flex-1">
+                    <div className="flex max-w-full p-4 items-center">
+                        <CardTitle className="flex-1">
+                            Balance Statistic
+                        </CardTitle>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button
+                                    variant="outline"
+                                    className="flex items-center gap-4"
+                                >
+                                    Filter
+                                    <ChevronDown size="16" />
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                                <DropdownMenuItem>Month</DropdownMenuItem>
+                                <DropdownMenuItem>Week</DropdownMenuItem>
+                                <DropdownMenuItem>Day</DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
+                </Card>
+                <Card className="w-[45%] bg-[#545DE6]">
                     <CardHeader>
-                        <CardTitle>Balance Statistic</CardTitle>
+                        <CardTitle>
+                            <Logo />
+                        </CardTitle>
                     </CardHeader>
                 </Card>
             </div>
